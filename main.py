82bd72d8b2437c4f1d5f4fee5e4d11ed1edb51e3
樓梯妹妹 <a0919329@gmail.com>
2022-03-23 12:56:04 +0800

    progress
    std_sort is useless, delete that garbage.

--- main.py
+++ main.py
@@ -1,49 +1,32 @@
 import json
 
-from compare import std_sorting
+import pandas as pd
 
-class fileoperations():
-    # open file, terminate externally
-    def softopen(filename:str, openmode:str):
-        filename += ".txt" # file extension handler
-        return open(filename, mode=openmode, encoding="utf-8")
+class operation():
+    def full_package(data, name:str):
+        with open(data, mode="r") as List:
+            with open(f"./output/{name}", mode="w") as package:
+                package.write(formatting(List))
 
-    def save(file:dict, filename:str):
-        with open(filename, mode="w") as file:
-            json.dump(file, filename)
-        return 1
+def softopen(filename:str, openmode:str):
+    filename += ".txt"
+    return open(filename, mode=openmode, encoding="utf-8")
 
-    def save_exit(file:dict, filename:str):
-        with open(filename, mode="w") as file:
-            file = json.load(file)
-            json.dump(file, filename)
-        file.close()
-        return 1
-
+# 設計class，一個用來整理成Series，一個用來整理成List
 def formatting(file):
-    tasks = []
+    mass_list = []
+    amplitude_list = []
     for line in file:
-
-        # define data format
         mass = float(line.split('\t')[0])
         amplitude = float(line.split('\t')[-1][:-1])
 
-        # create dict-like list for sorts
-        tasks.append((mass, amplitude))
-    file.close()
-    std_sorting(tasks)
+        mass_list.append(mass)
+        amplitude_list.append(amplitude)
+    tasks = pd.Series(data=amplitude_list, index=mass_list)
     return tasks
 
 def main():
-    # first manifest
-    datasets = []
-    data = fileoperations.softopen(filename="./raw/List1", openmode="r")
-    datasets.extend(formatting(data))
-
-    # second manifest
-    datasets = []
-    data = fileoperations.softopen(filename="./raw/List2", openmode="r")
-    datasets.extend(formatting(data))
+    operation.full_package(data="./raw/List1.txt", name="List1")
 
 if __name__ == "__main__":
     main()
